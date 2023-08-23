b3b462a30395339e17799e3842d806f1f5160add
x8xIsanamix8x <x8xgoncalvesx8x@gmail.com>
2023-08-23 19:34:46 -0400

    Jobslist - Advantages - initially footer
    I have finished with the list of jobs, finished the platform advantages section and started with the footer.

--- src/components/JobDiv/Jobs.jsx
+++ src/components/JobDiv/Jobs.jsx
@@ -3,12 +3,154 @@ import React from 'react'
 import{BiTimeFive} from 'react-icons/bi'
 
 import logo1 from "../../assets/logo1.png";
+import logo2 from "../../assets/logo2.png";
+import logo3 from "../../assets/logo3.png";
+import logo4 from "../../assets/logo4.png";
+import logo5 from "../../assets/logo5.png";
+import logo6 from "../../assets/logo6.png";
+import logo7 from "../../assets/logo7.png";
+import logo8 from "../../assets/logo8.png";
+import logo9 from "../../assets/logo9.png";
+import logo10 from "../../assets/logo10.png";
+
+const data = [{
+    id: 1,
+    image: logo1,
+    title: 'Web Developer',
+    time: 'Now',
+    location: 'Canada',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, placeat.',
+    company: 'Homebor Corp'
+  },
+  {
+    id: 2,
+    image: logo2,
+    title: 'UI Designer',
+    time: '14 hrs',
+    location: 'Miami',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, placeat.',
+    company: 'Google'
+  },
+  {
+    id: 3,
+    image: logo3,
+    title: 'Delivery Manager',
+    time: 'Now',
+    location: 'España',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, placeat.',
+    company: 'Burguer King'
+  },
+  {
+    id: 4,
+    image: logo4,
+    title: 'Technical Lead',
+    time: 'Now',
+    location: 'Mexico',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, placeat.',
+    company: 'Alcatel'
+  },
+  {
+    id: 5,
+    image: logo5,
+    title: 'Global Product Owner',
+    time: 'Now',
+    location: 'San Francisco',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, placeat.',
+    company: 'HomestayHome'
+  },
+  {
+    id: 6,
+    image: logo6,
+    title: 'UI Designer',
+    time: 'Now',
+    location: 'Miami',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, placeat.',
+    company: 'RockStarGames'
+  },
+  {
+    id: 7,
+    image: logo7,
+    title: 'Tester',
+    time: '20 hrs',
+    location: 'Portugal',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, placeat.',
+    company: 'Capcom'
+  },
+  {
+    id: 8,
+    image: logo8,
+    title: 'UI Designer',
+    time: 'Now',
+    location: 'Miami',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, placeat.',
+    company: 'Movistar'
+  },
+  {
+    id: 9,
+    image: logo9,
+    title: 'UX Experience',
+    time: '40 hrs',
+    location: 'Silicon Valley',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, placeat.',
+    company: 'Canaima GNU/LINUX'
+  },
+  {
+    id: 10,
+    image: logo10,
+    title: 'IT Account Manager',
+    time: '120 hrs',
+    location: 'Venezuela',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, placeat.',
+    company: 'Movistar'
+  }
+]
+
 
 export const Jobs = () => {
   return (
     <div>
       <div className="jobContainer flex gap-10 justify-center flex-wrap items-center py-10">
-        <div className="group group/item singleJob w-[250px] rounded-md p-[20px] bg-white rounded[10px] hover:bg-violet-500 shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
+
+        {
+          data.map(({ id, image, title, time, location, desc, company}) => {
+
+            return(
+              
+              <div key={id} className="group group/item singleJob w-[250px] rounded-md p-[20px] bg-white rounded[10px] hover:bg-violet-500 shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
+
+                <span className='flex justify-between items-center gap-4'>
+                    <h1 className='text-[16px] font-semibold text-textColor group-hover:text-white'>{title}</h1>
+                    <span className='flex items-center text-[#ccc] gap-1 '>
+                      <BiTimeFive/>{time}
+                    </span>
+                  </span>
+                <h6 className='text-[#ccc]'>{location}</h6>
+                <p className='text-[13px] text-[#959595] pt-[20px] border-t-[2px] mt-[20px] group-hover:text-white'
+                >{desc}
+                </p>
+
+                <div className='company flex items-center gap-2 m-2' >
+                  <img src={image} alt="Company Logo" className='w-[20%]' />
+                  <span className='text-[14px] py-[1rem] block group-hover:text-white '>{company}</span>
+                </div>
+
+                <button className='border-[2px] rounded-[10px] block p-[10px] w-full text-[14px] font-semibold text-textColor hover:bg-white group-hover/item:text-textColor group-hover:text-black'>
+                  Apply Now
+                </button>
+
+              </div>
+
+            )
+          })
+        }
+
+
+      </div>
+    </div>
+  )
+}
+
+{/* <div className="group group/item singleJob w-[250px] rounded-md p-[20px] bg-white rounded[10px] hover:bg-violet-500 shadow-lg shadow-greyIsh-400/700 hover:shadow-lg">
 
           <span className='flex justify-between items-center gap-4'>
               <h1 className='text-[16px] font-semibold text-textColor group-hover:text-white'>Web Developer</h1>
@@ -31,8 +173,4 @@ export const Jobs = () => {
           </button>
 
 
-        </div>
-      </div>
-    </div>
-  )
-}
+        </div> */}
--- src/components/ValueDiv/Value.jsx
+++ src/components/ValueDiv/Value.jsx
@@ -1,7 +1,67 @@
 import React from 'react'
 
+import fast from "../../assets/fast.png";
+import value from "../../assets/value.png";
+import money from "../../assets/money.webp";
+
+
+
 export const Value = () => {
   return (
-    <div>Value</div>
+    <div className='mb-[4rem] mt-[6rem]'>
+      <h1 className='text-textColor text-[25px] py-[2rem] pb-[3rem] font-bold w-[600px] block'
+      >The value that holds us true and to account</h1>
+
+      <div className='grid gap-[10rem] grid-cols-3 items-center'>
+        <div className='singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]'>
+          <div className='flex items-center gap-3'>
+            <div className='imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] h-[40] w-[40px] flex items-center justify-center'>
+              <img src={fast} alt='' className='w-[70%]' />
+            </div>
+
+            <span className='font-semibold text-textColor text-[18px]'>
+              Fast and Furious
+            </span>
+          </div>
+          <p className='text-[13px] text-textColor opacity-[.7] py-[1rem] font-semibold'>
+            Things being made beautiful simple are at the heart of everything we do.
+
+          </p>
+        </div>
+
+        <div className='singleGrid rounded-[10px] hover:bg-[#f7edf5] p-[1.5rem]'>
+          <div className='flex items-center gap-3'>
+            <div className='imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] h-[40] w-[40px] flex items-center justify-center'>
+              <img src={value} alt='' className='w-[70%]' />
+            </div>
+
+            <span className='font-semibold text-textColor text-[18px]'>
+              Create value to you company
+            </span>
+          </div>
+          <p className='text-[13px] text-textColor opacity-[.7] py-[1rem] font-semibold'>
+            Things being made beautiful simple are at the heart of everything we do.
+
+          </p>
+        </div>
+
+        <div className='singleGrid rounded-[10px] hover:bg-[#fcfae3] p-[1.5rem]'>
+          <div className='flex items-center gap-3'>
+            <div className='imgDiv p-[4px] rounded-[.8rem] bg-[#f3f2ad] h-[40] w-[40px] flex items-center justify-center'>
+              <img src={money} alt='' className='w-[70%]' />
+            </div>
+
+            <span className='font-semibold text-textColor text-[18px]'>
+              Fast and Furious
+            </span>
+          </div>
+          <p className='text-[13px] text-textColor opacity-[.7] py-[1rem] font-semibold'>
+            Things being made beautiful simple are at the heart of everything we do.
+
+          </p>
+        </div>
+
+      </div>
+    </div>
   )
 }
